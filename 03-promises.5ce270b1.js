eeb68489f9e456f6e52d8642c178b6b1b6100f83
yours-mate <106582125+yours-mate@users.noreply.github.com>
2022-11-03 11:08:53 +0000

    Deploying to gh-pages from @ yours-mate/goit-js-hw-09@b5a2ceae955e6dc7da82590537b97616e0131bd2 🚀

--- 03-promises.5ce270b1.js
+++
@@ -1,2 +0,0 @@
-refs={delay:document.querySelector('[name="delay"]'),step:document.querySelector('[name="step"]'),amount:document.querySelector('[name="amount"]'),submitFormBtn:document.querySelector('[type="submit"]')},console.log(refs.amount),console.log(refs.step);
-//# sourceMappingURL=03-promises.5ce270b1.js.map
